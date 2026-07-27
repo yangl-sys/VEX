f3e9835ee76c667772970d1893b82660a71b78d7
yangl-sys <yangl@pedarecc.sa.edu.au>
2026-07-27 11:51:12 +0930

    Commit 2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #region VEXcode Generated Robot Configuration
 from vex import *
-import urandom
 import math
 
 # Brain should be defined by default
@@ -15,26 +14,8 @@ Rightdtrain_motor_a = Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)
 Rightdtrain_motor_b = Motor(Ports.PORT19, GearSetting.RATIO_18_1, False)
 Rightdtrain = MotorGroup(Rightdtrain_motor_a, Rightdtrain_motor_b)
 
-
 # wait for rotation sensor to fully initialize
 wait(30, MSEC)
-
-
-# Make random actually random
-def initializeRandomSeed():
-    wait(100, MSEC)
-    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
-    urandom.seed(int(random))
-      
-# Set random seed 
-initializeRandomSeed()
-
-
-def play_vexcode_sound(sound_name):
-    # Helper to make playing sounds from the V5 in VEXcode easier and
-    # keeps the code cleaner by making it clear what is happening.
-    print("VEXPlaySound:" + sound_name)
-    wait(5, MSEC)
 
 # add a small delay to make sure we don't print in the middle of the REPL header
 wait(200, MSEC)
@@ -42,11 +23,6 @@ wait(200, MSEC)
 print("\033[2J")
 
 #endregion VEXcode Generated Robot Configuration
-
-def when_started1():
-    pass
-
-when_started1()
 
 def autonomous():
     pass
